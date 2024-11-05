e83cbc09a1ece7c9c5483a4724d919dd2c7bd68c
rafaelbvc <rafaelbvc@hotmail.com>
2024-11-05 14:27:30 -0300

    v.B-0.6 - Server att

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,19 @@
 import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+
+dotenv.config()
 
 const server = express()
 
-const port = 2020
+const port = process.env.PORT
 
 
-server.listen(port, () => {
-    console.log("Server is running on port: ", port)
-})
+server.use(cors({origin: "*"}))
+
+server.use(express.json())
+
+
+server.listen(port, () => 
+    console.log(`Server is running on : http://localhost:${port} `)
+)
