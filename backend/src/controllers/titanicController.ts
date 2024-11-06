f404df087dabbd24eb9dd593b368243b2afe68e7
rafaelbvc <rafaelbvc@hotmail.com>
2024-11-06 01:26:10 -0300

    v.B-1.2 - Controllers post

--- backend/src/controllers/titanicController.ts
+++ backend/src/controllers/titanicController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { request, Request, Response } from "express";
 import TitanicSchema from "../schemas/titanicSchema";
 
 export const getAll = async(request: Request, response: Response) => {
@@ -9,4 +9,16 @@ export const getAll = async(request: Request, response: Response) => {
     }
 
     response.send(passengerData).status(201).json({message: "Success"} )
+}
+
+export const createPassenger = async(request: Request, response: Response) => {
+
+    const { ids, passenger, survived, pClass, name, sex, age, sibSp, parch, ticket, fare, cabin, embarked } = request.body
+
+
+    const data = await TitanicSchema.create({ ids, passenger, survived, pClass, name, sex, age, sibSp, parch, ticket, fare, cabin, embarked})
+
+
+    response.status(201).json({message: "Passenger was successfully created!"})
+    console.log("Passenger was successfully created!", data)
 }
