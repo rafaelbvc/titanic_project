67a5cb2506f1a25baf7e958dad0ba539c03fecdd
rafaelbvc <rafaelbvc@hotmail.com>
2024-11-07 01:06:43 -0300

    v.B-1.3 - CRUD Complete

--- backend/src/routes/Router.ts
+++ backend/src/routes/Router.ts
@@ -1,5 +1,5 @@
 import {Router} from "express"
-import { createPassenger, getAll } from "../controllers/titanicController"
+import { createPassenger, deletePassenger, getAll, replacePassenger } from "../controllers/titanicController"
 
 
 const routes = Router()
@@ -7,5 +7,9 @@ const routes = Router()
 routes.get("/passenger", getAll)
 
 routes.post("/new/passenger", createPassenger)
+
+routes.put("/passenger/update/:ids", replacePassenger)
+
+routes.delete("/passenger/delete/:ids", deletePassenger)
 
 export default routes
